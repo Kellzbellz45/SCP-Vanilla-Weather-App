d4b8055cb4ec5255dd4b15a1cea1f6b1f0c07bd8
kellzbellz45 <kelly@kellyfaria.com>
2022-01-29 17:39:20 -0500

    Added a bunch of java

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "9b4aa1e9d9995bf996aaf55cc902b1e5";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+//-----Time/Day------//
+
+new Date(1572000098 + 1000);
