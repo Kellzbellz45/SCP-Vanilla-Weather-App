49c0e0676fc861373d42c7754f5a2e75f15fd1e7
kellzbellz45 <kelly@kellyfaria.com>
2022-01-29 18:25:47 -0500

    Time added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
+function formateDate(timestamp) {
+  // Calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minites < 10) {
+    minutes = `0${minutes}()`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDay();
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -13,6 +35,9 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "9b4aa1e9d9995bf996aaf55cc902b1e5";
@@ -22,5 +47,3 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${
 axios.get(apiUrl).then(displayTemperature);
 
 //-----Time/Day------//
-
-new Date(1572000098 + 1000);
