1a1614784931b815159a3bf7ed39079d5437bcbd
kellzbellz45 <kelly@kellyfaria.com>
2022-02-12 13:17:00 -0500

    HTML/CSS/JAVA update
    Removed the HTML for forecast to inject into JAVASCRIPT and put it into displayForecast function to create loop for all days of the week.

--- src/app.js
+++ src/app.js
@@ -18,8 +18,35 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
+
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
+}
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-sm-2">
+               <div class="weather-forecast-date">${day}</div>
+              <img src="http://openweathermap.org/img/wn/01d@2x.png"
+                  alt="Forecast"
+                  width="42"/>
+                 <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
@@ -94,3 +121,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 let celsiusTemperature = null;
+
+displayForecast();
